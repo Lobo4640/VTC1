ecf82bb6e02a9ca8c3372a71cb3823d2b5086106
Lobo4640 <lrm-18@msn.com>
2026-04-07 11:00:38 +0200

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -10,7 +10,7 @@
 import { useState, useEffect } from "react";
 import { useRouter }           from "next/navigation";
 import { createClient }        from "@supabase/supabase-js";
-
+export const dynamic = "force-dynamic";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
